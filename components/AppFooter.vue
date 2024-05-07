199d017d589ad9d8e3c77058f45bc0c668587f4d
Frenchie27 <frenchie27@gmail.com>
2024-05-07 14:37:40 +0200

    Added styles to the AppFooter and finished cleaning up the main scss file

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,7 +5,14 @@
 </template>
 
 <style lang="scss" scoped>
+@import './assets/css/_variables.scss';
+
 footer {
+  width: 100%;
   height: 50px;
+  z-index: 5;
+  padding: 10px 30px;
+  color: $primary;
+  text-align: center;
 }
 </style>
