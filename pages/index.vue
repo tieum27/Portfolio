cc2df6e0ae7cecb665b296b2738a6037c28558bc
Frenchie27 <frenchie27@gmail.com>
2024-05-07 11:48:45 +0200

    Added styles to the index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,3 @@
-<template>
-  <main class="tab-content">
-    <Home :isActive="isActive(1)" :setShowModal="setShowModal" />
-    <About :isActive="isActive(2)" />
-    <Portfolio
-      :isActive="isActive(3)"
-      :setPortfolioDetails="setPortfolioDetails"
-    />
-    <Contact :isActive="isActive(4)" />
-  </main>
-</template>
-
 <script>
 import Home from "../components/tabs/Home.vue";
 import About from "../components/tabs/About.vue";
@@ -27,3 +15,43 @@ export default {
   },
 };
 </script>
+
+<template>
+  <main class="tab-content">
+    <Home :isActive="isActive(1)" :setShowModal="setShowModal" />
+    <About :isActive="isActive(2)" />
+    <Portfolio
+    :isActive="isActive(3)"
+    :setPortfolioDetails="setPortfolioDetails"
+    />
+    <Contact :isActive="isActive(4)" />
+  </main>
+</template>
+
+<style scoped lang="scss">
+.tab-content > .page {
+  display: none;
+  opacity: 0;
+  visibility: hidden;
+  overflow: hidden;
+  backface-visibility: hidden;
+  -webkit-transition: opacity 2s ease-out;
+  -moz-transition: opacity 2s ease-out;
+  -o-transition: opacity 2s ease-out;
+  transition: opacity 2s ease-out;
+}
+
+.tab-content > .active-page {
+  display: block;
+  opacity: 1;
+  visibility: visible;
+  // animation: scaleDownUp 0.5s ease both;
+  overflow: hidden;
+}
+
+main {
+  display: flex;
+  overflow: hidden;
+  min-height: calc(100vh - 40px);
+}
+</style>
