7f30b20e0c123776533c9141ec8cbc566d854a38
Frenchie27 <frenchie27@gmail.com>
2024-05-07 14:19:33 +0200

    Added styles to the portfolio card

--- components/cards/Portfolio.vue
+++ components/cards/Portfolio.vue
@@ -1,3 +1,10 @@
+<script>
+export default {
+  name: "PortfolioCard",
+  props: ["avatar", "title", "description"],
+};
+</script>
+
 <template>
   <figure class="portfolio-card">
     <img :src="require('@/assets/' + avatar)" />
@@ -9,9 +16,59 @@
     </div>
   </figure>
 </template>
-<script>
-export default {
-  name: "PortfolioCard",
-  props: ["avatar", "title", "description"],
-};
-</script>
+
+<style lang="scss">
+.portfoliopage {
+  .projects-list {
+    margin: 0 auto;
+    justify-content: center;
+    padding: 0;
+    li {
+      width: 33.333333%;
+      float: left;
+      cursor: pointer;
+      padding: 15px;
+    }
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .portfoliopage {
+    .projects-list {
+      li {
+        width: 100%;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 991px) {
+  .portfoliopage {
+    .projects-list {
+      grid-template-columns: none !important;
+      padding: 0 !important;
+    }
+  }
+}
+
+@media screen and (min-width: 577px) and  (max-width: 1200px) {
+  .portfoliopage {
+    .projects-list {
+      li {
+        width: 50%;
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 1300px) {
+  .portfoliopage {
+    .projects-list {
+      figure {
+        min-height: 212px;
+        background: #ddd;
+      }
+    }
+  }
+}
+</style>
